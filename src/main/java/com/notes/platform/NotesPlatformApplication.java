c5a30c4a2e3210c3c49f90e73100d56a30a7a039
Shubham Srivastava <shubhamjava1987@gmail.com>
2025-12-29 08:41:56 -0800

    first commit

---
+++ src/main/java/com/notes/platform/NotesPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.notes.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesPlatformApplication.class, args);
+	}
+
+}
